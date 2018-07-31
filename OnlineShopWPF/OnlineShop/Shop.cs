5163b6ff5844ac53dc75b207caa656a86f7c1ce4
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-07-31 14:55:40 +0200

    Created Cpu Object with Constructor, use parameterized query to create the cpu in the entry in the databse

--- OnlineShopWPF/OnlineShop/Shop.cs
+++ OnlineShopWPF/OnlineShop/Shop.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Globalization;
 
 namespace OnlineShop
 {
   public class Shop
   {
+
+  }
+
+  public class CPU
+  {
+    SQLiteConnection connection;
+    public CPU(int count, float clockRate, string name)
+    {
+      using (connection = new SQLiteConnection(@"Data Source = C:\Users\jsiedersberger\Documents\GitHub\OnlineShopWPF\OnlineShopWPF\OnlineShop.db; Version=3"))
+      {
+        connection.Open();
+        SQLiteCommand command = new SQLiteCommand(connection);
+        command.CommandText = "INSERT INTO Cpu(cpu_id, count, clock_rate, name) VALUES(?,?,?,?) ";
+        command.Parameters.AddWithValue("param1", null);
+        command.Parameters.AddWithValue("param2", count);
+        command.Parameters.AddWithValue("param3", clockRate);
+        command.Parameters.AddWithValue("param4", name);
+
+
+
+
+
+        //command.CommandText = String.Format("INSERT INTO Cpu(cpu_id, count, clock_rate, name) VALUES (NULL,{0},{1},\"{2}\")", count, clockRate.ToString(CultureInfo.InvariantCulture), name);
+        command.ExecuteNonQuery();
+
+      }
+    }
   }
 }
--- OnlineShopWPF/OnlineShopConsoleApp/Program.cs
+++ OnlineShopWPF/OnlineShopConsoleApp/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SQLite;
+using OnlineShop;
 namespace OnlineShopConsoleApp
 {
   class Program
   {
     static void Main(string[] args)
     {
+      CPU c = new CPU(777777, 3.5f, "INTEL PENTIUM");
     }
   }
 }
