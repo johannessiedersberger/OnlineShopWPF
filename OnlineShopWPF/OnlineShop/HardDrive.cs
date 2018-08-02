962e7341a6a61cb3a91e411678fd1eb64d0d500a
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-08-02 15:48:40 +0200

    HardDrive: added Comments, used Interface iDatabase

--- OnlineShopWPF/OnlineShop/HardDrive.cs
+++ OnlineShopWPF/OnlineShop/HardDrive.cs
@@ -9,16 +9,30 @@ namespace OnlineShop
 {
   public class HardDrive
   {
+    /// <summary>
+    /// The Memory of the HardDisk in GB
+    /// </summary>
     public int Memory { get; private set; }
+    /// <summary>
+    /// The type of the HardDisk (SSD or HDD)
+    /// </summary>
     public string Type { get; private set; }
 
+    IDatabase database = new SqliteDatabase("OnlineShop.db");
+
+    /// <summary>
+    /// Creates a HardDrive in the Database
+    /// </summary>
+    /// <param name="memory"></param>
+    /// <param name="type"></param>
     public HardDrive(int memory, string type)
     {
+      if (CheckIfHardDriveExists(memory, type))
+        return;
+
       Memory = memory;
       Type = type;
-
-      SqliteDatabase database = new SqliteDatabase("OnlineShop.db");
-
+      
       using (var createHardDrive = database.CreateCommand(CommandAddHardDrive))
       {
         database.Open();
@@ -26,46 +40,69 @@ namespace OnlineShop
         createHardDrive.Parameters.Add(database.CreateParameter("$type", type));
         createHardDrive.Parameters.Add(database.CreateParameter("$memory", memory.ToString()));
         createHardDrive.ExecuteNonQuery();
+
         database.Close();
       }
     }
 
     private const string CommandAddHardDrive = "INSERT INTO HardDrives(hard_drive_id, type, memory) VALUES($id,$type,$memory)";
 
-    public static bool CheckIfHardDriveExists(int memory, string type)
+    /// <summary>
+    /// Checks if the HardDrive already exists in the Databse
+    /// </summary>
+    /// <param name="memory">memory in GB</param>
+    /// <param name="type">ssd or hdd</param>
+    /// <returns></returns>
+    public bool CheckIfHardDriveExists(int memory, string type)
     {
-      using (SQLiteConnection connection = new SQLiteConnection(@"Data Source = C:\Users\jsiedersberger\Documents\GitHub\OnlineShopWPF\OnlineShopWPF\OnlineShop.db; Version=3"))
+      
+      using (var checkHardDrive = database.CreateCommand(CommandCheckHardDrive))
       {
-        connection.Open();
-        SQLiteCommand command = new SQLiteCommand(connection);
-        command.CommandText = "SELECT memory, type FROM HardDrives WHERE memory = $memory AND type = $type";
-        command.Parameters.AddWithValue("$type", type);
-        command.Parameters.AddWithValue("$memory", memory);
-        SQLiteDataReader reader = command.ExecuteReader();
+        database.Open();
+        checkHardDrive.Parameters.Add(database.CreateParameter("$type", type));
+        checkHardDrive.Parameters.Add(database.CreateParameter("$memory", memory.ToString()));
+
+        var reader = checkHardDrive.ExecuteReader();
         while (reader.Read())
+        {
           if (reader[0].ToString() != null)
+          {
+            database.Close();
             return true;
+          }
+        }
+        database.Close();
         return false;
       }
     }
 
-    public static int GetId(int memory, string type)
+    private const string CommandCheckHardDrive = "SELECT memory, type FROM HardDrives WHERE memory = $memory AND type = $type";
+
+    /// <summary>
+    /// Returns the id from the Databse 
+    /// </summary>
+    /// <param name="memory"></param>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public int GetId(int memory, string type)
     {
-      SQLiteConnection connection;
-      using (connection = new SQLiteConnection(@"Data Source = C:\Users\jsiedersberger\Documents\GitHub\OnlineShopWPF\OnlineShopWPF\OnlineShop.db; Version=3"))
+      database.Open();
+      using (var getID = database.CreateCommand(CommandSelectID))
       {
-        connection.Open();
-        SQLiteCommand command = new SQLiteCommand(connection);
-        command.CommandText = "SELECT hard_drive_id FROM HardDrives WHERE memory = $memory AND type = $type";
-        command.Parameters.AddWithValue("$memory", memory);
-        command.Parameters.AddWithValue("$type", type);
-        SQLiteDataReader reader = command.ExecuteReader();
+        
+        getID.Parameters.Add(database.CreateParameter("$memory", memory.ToString()));
+        getID.Parameters.Add(database.CreateParameter("$type", type));
+        var reader = getID.ExecuteReader();
 
         int id = 0;
         while (reader.Read())
           id = int.Parse(reader[0].ToString());
+        database.Close();
         return id;
       }
     }
+
+    private const string CommandSelectID = "SELECT hard_drive_id FROM HardDrives WHERE memory = $memory AND type = $type";
   }
+
 }
