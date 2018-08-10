2ce3762f29acf55de606a67528380ff856d2fb48
Johannes Siedersberger <36839962+johannessiedersberger@users.noreply.github.com>
2018-08-10 11:51:57 +0200

    Graphic: refactor

--- OnlineShopWPF/OnlineShop/Graphic.cs
+++ OnlineShopWPF/OnlineShop/Graphic.cs
@@ -1,75 +1,64 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Data.SQLite;
-//using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
 
-//namespace OnlineShop
-//{
-//  public class Graphic
-//  {
-//    /// <summary>
-//    /// The Video RAM of the Graphic-Card in GB
-//    /// </summary>
-//    public int VRAM { get; private set; }
-//    /// <summary>
-//    /// The name of the Graphic-Card
-//    /// </summary>
-//    public string Name { get; private set; }
+namespace OnlineShop
+{
+  public class Graphic
+  {
+    /// <summary>
+    /// The Video RAM of the Graphic-Card in GB
+    /// </summary>
+    public int VRAM { get; private set; }
+    /// <summary>
+    /// The name of the Graphic-Card
+    /// </summary>
+    public string Name { get; private set; }
 
-//    private IDatabase database = new SqliteDatabase("OnlineShop.db");
+    private IDatabase _database;
+    /// <summary>
+    /// Creates a Graphic-Card in the Database
+    /// </summary>
+    /// <param name="vram">The video RAM of the Graphic-Card</param>
+    /// <param name="name">The name of the Graphic-card</param>
+    public Graphic(IDatabase db, int vram, string name)
+    {
+      VRAM = vram;
+      Name = name;
+      _database = db;
 
-//    /// <summary>
-//    /// Creates a Graphic-Card in the Database
-//    /// </summary>
-//    /// <param name="vram">The video RAM of the Graphic-Card</param>
-//    /// <param name="name">The name of the Graphic-card</param>
-//    public Graphic(int vram, string name)
-//    {
-//      VRAM = vram;
-//      Name = name;
+      using (var createGraphic = _database.CreateNonQueryCommand(CommandAddGraphic))
+      {
+        createGraphic.AddParameter("$id", null);
+        createGraphic.AddParameter("$vram", vram.ToString());
+        createGraphic.AddParameter("$name", name);
+        createGraphic.Execute();
+      }
+    }
 
-//      if (GetId(name) != 0)
-//        return;
+    private const string CommandAddGraphic = "INSERT INTO Graphics(graphic_id, vram, name) VALUES($id,$vram,$name)";
 
-//      using (var createGraphic = database.CreateQueryCommand(CommandAddGraphic))
-//      {
-//        database.Open();
-//        createGraphic.Parameters.Add(database.CreateParameter("$id", null));
-//        createGraphic.Parameters.Add(database.CreateParameter("$vram", vram.ToString()));
-//        createGraphic.Parameters.Add(database.CreateParameter("$name", name));
-//        createGraphic.ExecuteNonQuery();
-//        database.Dispose();
-//      }
-//    }
+    /// <summary>
+    /// Gets the ID from the Graphic-Card
+    /// </summary>
+    public int Id
+    {
+      get
+      {
+        using(var getID = _database.CreateQueryCommand(CommandSelectID))
+        {
+          getID.AddParameter("$name", Name);
+          IReader reader = getID.ExecuteReader();
+          return Convert.ToInt16(reader[0]);
+        }
+      }
+    }
 
-//    private const string CommandAddGraphic = "INSERT INTO Graphics(graphic_id, vram, name) VALUES($id,$vram,$name)";
+    private const string CommandSelectID = "SELECT graphic_id FROM Graphics WHERE name = $name";
 
-//    /// <summary>
-//    /// Returns the id from the Databse 
-//    /// </summary>
-//    /// <param name="memory"></param>
-//    /// <param name="type"></param>
-//    /// <returns></returns>
-//    public int GetId(string name)
-//    {
-//      using (var getID = database.CreateQueryCommand(CommandSelectID))
-//      {
-//        database.Open();
-//        getID.Parameters.Add(database.CreateParameter("$name", name));
-//        IDataReader reader = getID.ExecuteReader();
-
-//        int id = 0;
-//        while (reader.Read())
-//          id = int.Parse(reader[0].ToString());
-//        database.Dispose();
-//        return id;
-//      }
-//    }
-
-//    private const string CommandSelectID = "SELECT graphic_id FROM Graphics WHERE name = $name";
-
-//  }
-//}
+  }
+}
